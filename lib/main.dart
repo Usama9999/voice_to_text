94681496535d6061270bb78f86e942113e738c1c
musman9 <Muhammad.usman@geappliances.com>
2022-10-26 23:47:59 +0500

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,177 @@
-import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_speech/flutter_speech.dart';
 
 void main() {
-  runApp(MyApp());
+  debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  runApp(const MyApp());
 }
+
+class Language {
+  final String name;
+  final String code;
+
+  const Language(this.name, this.code);
+}
+
+const languages = [
+  Language('English', 'en_US'),
+];
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Voice',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: SpeechScreen(),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: VoiceSearchingDemo());
   }
 }
 
-class SpeechScreen extends StatefulWidget {
+class VoiceSearchingDemo extends StatefulWidget {
+  const VoiceSearchingDemo({Key? key}) : super(key: key);
+
   @override
-  _SpeechScreenState createState() => _SpeechScreenState();
+  _VoiceSearchingDemoState createState() => _VoiceSearchingDemoState();
 }
 
-class _SpeechScreenState extends State<SpeechScreen> {
-  late stt.SpeechToText _speech;
+class _VoiceSearchingDemoState extends State<VoiceSearchingDemo> {
+  late SpeechRecognition _speech;
+
+  bool _speechRecognitionAvailable = false;
   bool _isListening = false;
-  String _text = 'Press the button and start speaking';
-  double _confidence = 1.0;
+
+  String transcription = '';
+
+  //String _currentLocale = 'en_US';
+  Language selectedLang = languages.first;
 
   @override
-  void initState() {
+  initState() {
     super.initState();
-    _speech = stt.SpeechToText();
+    activateSpeechRecognizer();
   }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  void activateSpeechRecognizer() {
+    print('_MyAppState.activateSpeechRecognizer... ');
+    _speech = SpeechRecognition();
+    _speech.setAvailabilityHandler(onSpeechAvailability);
+    _speech.setRecognitionStartedHandler(onRecognitionStarted);
+    _speech.setRecognitionResultHandler(onRecognitionResult);
+    _speech.setRecognitionCompleteHandler(onRecognitionComplete);
+    _speech.setErrorHandler(errorHandler);
+    _speech.activate('en_US').then((res) {
+      setState(() => _speechRecognitionAvailable = res);
+    });
+  }
+
+  Widget _buildButton({required String label, VoidCallback? onPressed}) =>
+      Padding(
+          padding: EdgeInsets.all(12.0),
+          child: ElevatedButton(
+            onPressed: onPressed,
+            child: Text(
+              label,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ));
+
+  void start() => _speech.activate(selectedLang.code).then((_) {
+        return _speech.listen().then((result) {
+          print('_MyAppState.start => result $result');
+          setState(() {
+            _isListening = result;
+          });
+        });
+      });
+
+  void cancel() =>
+      _speech.cancel().then((_) => setState(() => _isListening = false));
+
+  void stop() => _speech.stop().then((_) {
+        setState(() => _isListening = false);
+      });
+
+  void onCurrentLocale(String locale) {
+    print('_MyAppState.onCurrentLocale... $locale');
+    setState(
+        () => selectedLang = languages.firstWhere((l) => l.code == locale));
+  }
+
+  void onSpeechAvailability(bool result) =>
+      setState(() => _speechRecognitionAvailable = result);
+
+  void onRecognitionStarted() {
+    setState(() => _isListening = true);
+  }
+
+  void onRecognitionResult(String text) {
+    print('_MyAppState.onRecognitionResult... $text');
+    setState(() => transcription = text);
+  }
+
+  void onRecognitionComplete(String text) {
+    print('_MyAppState.onRecognitionComplete... $text');
+    setState(() => _isListening = false);
+  }
+
+  void errorHandler() => activateSpeechRecognizer();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Accuracy: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+        automaticallyImplyLeading: false,
+        title: const Text('Flutter Voice Searching Demo'),
+        actions: [
+          PopupMenuButton<Language>(
+            onSelected: _selectLangHandler,
+            itemBuilder: (BuildContext context) => _buildLanguagesWidgets,
+          )
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: AvatarGlow(
-        animate: _isListening,
-        glowColor: Theme.of(context).primaryColor,
-        endRadius: 75.0,
-        duration: const Duration(milliseconds: 2000),
-        repeatPauseDuration: const Duration(milliseconds: 100),
-        repeat: true,
-        child: FloatingActionButton(
-          onPressed: _listen,
-          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-        ),
-      ),
-      body: SingleChildScrollView(
-        reverse: true,
-        child: Container(
-            padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
-            child: Text(_text)),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                    child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        color: Colors.grey.shade200,
+                        child: Text(transcription))),
+                _buildButton(
+                  onPressed: _speechRecognitionAvailable && !_isListening
+                      ? () => start()
+                      : null,
+                  label: _isListening ? 'Listening...' : 'start',
+                ),
+                _buildButton(
+                  onPressed: _isListening ? () => cancel() : null,
+                  label: 'Cancel',
+                ),
+                _buildButton(
+                  onPressed: _isListening ? () => stop() : null,
+                  label: 'Stop',
+                ),
+              ],
+            ),
+          )),
     );
   }
 
-  void _listen() async {
-    if (!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus: $val'),
-        onError: (val) => print('onError: $val'),
-      );
-      if (available) {
-        setState(() => _isListening = true);
-        _speech.listen(
-          onResult: (val) => setState(() {
-            _text = val.recognizedWords;
-            if (val.hasConfidenceRating && val.confidence > 0) {
-              _confidence = val.confidence;
-            }
-          }),
-        );
-      }
-    } else {
-      setState(() => _isListening = false);
-      _speech.stop();
-    }
+  List<CheckedPopupMenuItem<Language>> get _buildLanguagesWidgets => languages
+      .map((l) => CheckedPopupMenuItem<Language>(
+            value: l,
+            checked: selectedLang == l,
+            child: Text(l.name),
+          ))
+      .toList();
+
+  void _selectLangHandler(Language lang) {
+    setState(() => selectedLang = lang);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import speech_to_text_macos
+import flutter_speech
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  FlutterSpeechRecognitionPlugin.register(with: registry.registrar(forPlugin: "FlutterSpeechRecognitionPlugin"))
 }
